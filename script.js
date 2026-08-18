b92032b0785aac91916c80b81045b6ed886ce54d
dev-masha <mashadev.git@gmail.com>
2026-08-18 18:18:04 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+const BASE_URL = "https://api.exchangerate-api.com/v4/latest/GBP";
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
+
+btn.addEventListener("click", async (evt) => {
+    evt.preventDefault();
+    let amount = document.querySelector(".amount input");
+    let amtVal = amount.value;
+    if (amtVal === "" || amtVal < 1) {
+        amtVal = 1;
+        amount.value = "1";
+    }
+    let response = await fetch(BASE_URL);
+    let data = await response.json();
+    let rate = data.rates.PKR;
+    let finalAmount = amtVal * rate;
+    msg.innerText = `${amtVal} GBP = ${finalAmount.toFixed(2)} PKR`;
+});
